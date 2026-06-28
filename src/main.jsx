acae9160eb36743d4bc7d9afe6257d2facc5ae20
ludodigitalmkt-prog <ludodigitalmkt@gmail.com>
2026-06-28 18:39:16 +0000

    Corrige main jsx do site Epigenics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-mkdir -p src
-cat > src/main.jsx <<'EOF'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-EOF
